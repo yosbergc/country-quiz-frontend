66f34c2e30bd9aba8c3769c4363aced2450549e7
Yosber Guerrero <yosbergc@gmail.com>
2024-12-26 17:52:51 -0400

    Added dynamic check or close for answers in questions

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,5 +1,8 @@
 import './question.css'
+import Check from '../../assets/Check_round_fill.svg'
+import Close from '../../assets/Close_round_fill.svg'
 function Question({ quiz, setAnswer }) {
+    
     return (
         <section className='single-question--container'>
             <h2
@@ -9,12 +12,21 @@ function Question({ quiz, setAnswer }) {
             <section className="options">
                 {
                     quiz.questions[quiz.currentQuestion - 1].options.map(answer => {
+                        const dynamicClass = `option-single ${quiz.questions[quiz.currentQuestion - 1].answer === answer && 'option-single-active'}`
+                        const correctAnswer = quiz.questions[quiz.currentQuestion - 1].answer === quiz.questions[quiz.currentQuestion - 1].correctAnswer && answer === quiz.questions[quiz.currentQuestion - 1].answer;
+                        const incorrectAnswer = quiz.questions[quiz.currentQuestion - 1].answer !== quiz.questions[quiz.currentQuestion - 1].correctAnswer && answer === quiz.questions[quiz.currentQuestion - 1].answer;
                         return <button
                         key={answer}
-                        className={quiz.questions[quiz.currentQuestion - 1].answer === answer && 'option-single option-single-active'}
+                        className={dynamicClass}
                         onClick={() => setAnswer(quiz.currentQuestion - 1, answer)}
                         >
                           {answer}
+                          {
+                            correctAnswer && <img src={Check} alt="Correct Answer" />
+                          }
+                          {
+                            incorrectAnswer && <img src={Close} alt='Incorrect answer'/>
+                          }
                         </button>
                     })
                 }
