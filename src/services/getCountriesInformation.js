aad8c1dfbf42ddae973dc8a14a4f03907d078395
Yosber Guerrero <yosbergc@gmail.com>
2025-01-01 17:11:14 -0400

    Changed service to get countries

--- src/services/getCountriesInformation.js
+++ src/services/getCountriesInformation.js
@@ -1,11 +1,17 @@
 function getCountries() {
     return fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        return new Error('There was an error with the request.')
+      }
+
+      return response.json()
+    })
     .then(data => {
-      console.log(data)
+      return data;
     })
     .catch(error => {
-      console.log(error)
+      return new Error(error)
     })
 }
 export { getCountries }
